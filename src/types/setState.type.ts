caf840b89ecf5e269abd2ce0d81bdba220487468
AndreyNedelevich <nedelevich@gmail.com>
2023-04-22 11:18:38 +0300

    HomeWork4TS

--- src/types/setState.type.ts
+++ src/types/setState.type.ts
@@ -1,5 +1,11 @@
 import {Dispatch, SetStateAction, useState} from 'react';
+import {IUser} from "../interfaces/user.interface";
 
 export type ISetState<T> = Dispatch<SetStateAction<T>>
 
-//В данном файле типизация
+//В данном файле типизация для повторного использования в Компонентах. Если мы передаем функцию дтспетчер для изменеия
+//состояния мы типизируем эту функцию в получаемом компоненте в Iprops.
+// interface IProps {
+//     user: IUser;
+
+// }
