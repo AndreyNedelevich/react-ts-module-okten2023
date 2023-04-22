b942405245556c26e87a78966e52672f012bba23
AndreyNedelevich <nedelevich@gmail.com>
2023-04-22 11:04:49 +0300

    HomeWork4TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import {Navigate, Route, Routes} from 'react-router-dom';
+import {MainLayout} from './layouts/MainLayout';
+import {UsersPage} from './pages/UsersPage';
+import {UserDetailsPage} from './pages/UserDetailsPage';
 
-function App() {
-  return (
-    <div>
-
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route index element={<Navigate to={'users'}/>}/>
+                <Route path={'users'} element={<UsersPage/>}>
+                    <Route path={':id'} element={<UserDetailsPage/>}/>
+                </Route>
+            </Route>
+        </Routes>
+    );
+};
 
 export default App;
