7a2b295e5559fcc7d8720aafbfcf564fab1e3cdf
AndreyNedelevich <nedelevich@gmail.com>
2023-05-06 11:04:50 +0300

    lesson_8_reduxToolkit_ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import './App.css';
+import {Navigate, Route, Routes} from 'react-router-dom';
 
-function App() {
-  return (
-    <div>
+import {MainLayout} from './layouts';
+import {CarPage} from './pages';
 
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route index element={<Navigate to={'cars'}/>}/>
+                <Route path={'cars'} element={<CarPage/>}/>
+            </Route>
+        </Routes>
+    );
+};
 
 export default App;
